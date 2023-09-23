dcbf6ad3f889adbf76f2f407ce8420f052d3b600
shubham <shubham30898@gmail.com>
2023-09-23 12:46:12 +0530

    output binding send event from product-details to product-list

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+// The Output artifact is an Angular property decorator that is used when we want to create 
+// events that will be triggered from one component up to another. The EventEmitter class 
+// is used to emit those events.
 
 @Component({
   selector: 'app-product-detail',
@@ -9,5 +13,14 @@ export class ProductDetailComponent {
 
   @Input() 
   name = '';
+
+  @Output() 
+  bought = new EventEmitter();
+
+  // The buy method calls the emit method on the bought output event we created in the previous step. The emit
+  //  method emits an event and notifies any component currently  listening to the event.
+  buy() {
+    this.bought.emit();
+   }
 
 }
